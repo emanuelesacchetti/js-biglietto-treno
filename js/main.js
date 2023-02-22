111a8a005b005977bc49c6899223be3b36746085
emanuele sacchetti <manusacco92@gmail.com>
2023-02-22 16:39:20 +0100

    aggiunto un po' di css e aggiunta una frase di spiegazione in caso di scontistica

--- js/main.js
+++ js/main.js
@@ -40,6 +40,22 @@ let costoBiglietto;
     costoBiglietto = prezzoTratta
  }
 
+
+const fraseMinorenni = 'Ti è stato applicato uno sconto del 20% in quanto minorenne';
+const fraseOver = 'Ti è stato applicato uno sconto del 40% in quanto over65';
+
+
+ if (età < 18) {
+    document.getElementById('why').innerHTML = fraseMinorenni;
+ } else if (età > 65) {
+    document.getElementById('why').innerHTML = fraseOver;
+ } else {
+    document.getElementById('why').innerHTML = 'BUON VIAGGIO';
+ }
+
 //inserisco il risultato all'interno dell'html
 
  document.getElementById('biglietto').innerHTML = costoBiglietto;
+
+ document.getElementById('eta-utente').innerHTML = età;
+ document.getElementById('km-utente').innerHTML = numeroKm;
