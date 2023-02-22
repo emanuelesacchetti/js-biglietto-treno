a7104fabeba859047adc535892bd6d962f52d942
emanuele sacchetti <manusacco92@gmail.com>
2023-02-22 15:26:57 +0100

    l'esercizio sembra funzionare bene. ora manca un po' di stile

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//chiedo all'utente quanti km farà e quanti anni ha con PROMPT
+//trasformo i valori dati come stringa in numeri con PARSEINT
+
+const numeroKm = parseInt(prompt('Quanti km vuoi percorrere'));
+const età = parseInt(prompt('Quanti anni hai?'));
+
+//definisco in variabile il prezzo di un singolo km
+//definisco in variabile la moltiplicazione dei km e del prezzo di 1 km
+
+let prezzoKm = 0.21;
+let prezzoTratta = (numeroKm * prezzoKm);
+
+ console.log(prezzoTratta);
+
+ //definisco in variabili gli sconti per minorenni e over65
+ //creo una variabile con il costo del bilgietto con sconto applicato
+
+ let scontoMinorenni = ((prezzoTratta * 20) / 100);
+ let prezzoMinorenni = prezzoTratta - scontoMinorenni;
+
+ console.log(scontoMinorenni);
+ console.log(prezzoMinorenni);
+
+ let scontoOver = ((prezzoTratta * 40) / 100);
+ let prezzoOver = prezzoTratta - scontoOver;
+
+ console.log(scontoOver);
+ console.log(prezzoOver);
+
+ //dichiaro che normalmente la var costoBiglietto è uguale a prezzoTratta
+ //ma nel caso di alcune condizioni il suo valore cambia
+
+let costoBiglietto = prezzoTratta
+
+ if (età < 18) {
+    costoBiglietto = prezzoMinorenni
+ } else if (età > 65) {
+    costoBiglietto = prezzoOver
+ } else
+
+//inserisco il risultato all'interno dell'html
+
+ document.getElementById('biglietto').innerHTML = costoBiglietto;
